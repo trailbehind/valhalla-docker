71bc81f3d4936a32769e2155d852ee8eb3476846
jczaplew <john@gaiagps.com>
2019-05-21 14:18:35 -0500

    Update with v3 install config

--- scripts/install_from_source.sh
+++ scripts/install_from_source.sh
@@ -1,48 +1,50 @@
 #!/usr/bin/env bash
 set -e
 
-# get all the ppas we might need
 add-apt-repository -y ppa:valhalla-core/valhalla
 apt-get update -y
-
-# get all the dependencies might need
 apt-get install -y \
-  git \
-  autoconf \
-  automake \
-  make \
-  libtool \
-  pkg-config \
-  g++ \
-  gcc \
-  jq \
-  lcov \
-  protobuf-compiler \
-  vim-common \
-  libboost-all-dev \
-  libboost-all-dev \
-  libcurl4-openssl-dev \
-  zlib1g-dev \
-  liblz4-dev \
-  libprime-server0.6.3-dev \
-  libprotobuf-dev prime-server0.6.3-bin \
-  libgeos-dev \
-  libgeos++-dev \
-  liblua5.2-dev \
-  libspatialite-dev \
-  libsqlite3-dev \
-  spatialite-bin \
-  wget \
-  unzip \
-  lua5.2 \
-  locales \
-  python-all-dev
+    curl \
+    cmake \
+    make \
+    libtool \
+    pkg-config \
+    g++ \
+    gcc \
+    git \
+    jq \
+    lcov \
+    protobuf-compiler \
+    vim-common \
+    libboost-all-dev \
+    libboost-all-dev \
+    libcurl4-openssl-dev \
+    zlib1g-dev \
+    liblz4-dev \
+    libprime-server0.6.3-dev \
+    libprotobuf-dev \
+    prime-server0.6.3-bin \
+    nodejs \
+    npm \
+    libgeos-dev \
+    libgeos++-dev \
+    liblua5.2-dev \
+    libspatialite-dev \
+    libsqlite3-dev \
+    lua5.2 \
+    wget \
+    python-all-dev
 
 if [[ $(grep -cF xenial /etc/lsb-release) > 0 ]]; then
-  apt-get install -y libsqlite3-mod-spatialite
+    apt-get install -y libsqlite3-mod-spatialite;
 fi
 
-# get the software installed
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 10 && nvm use 10
+
 git clone \
   --depth=1 \
   --recurse-submodules \
@@ -51,34 +53,11 @@ git clone \
   https://github.com/trailbehind/valhalla.git libvalhalla
 
 cd libvalhalla
-./autogen.sh
-./configure --enable-static
+
+git submodule update --init --recursive
+npm install --ignore-scripts
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DENABLE_DATA_TOOLS=On -DENABLE_SERVICES=On -DBUILD_SHARED_LIBS=On -DENABLE_PYTHON_BINDINGS=On
 make -j$(nproc)
-make install
-cd -
-
-# clean up
-ldconfig
-rm -rf libvalhalla
-
-#osmlr
-add-apt-repository ppa:valhalla-core/opentraffic
-apt-get update -y
-
-# get the software installed
-git clone \
-  --depth=1 \
-  --recurse-submodules \
-  --single-branch \
-  --branch=master \
-  https://github.com/opentraffic/osmlr.git osmlr
-
-cd osmlr
-./autogen.sh
-./configure --enable-static
-make -j$(nproc)
-make install
-cd -
-
-# clean up
-rm -rf osmlr
+sudo make install
